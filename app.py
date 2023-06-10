e0e5b94d9083278c8be43072dc4a0f5f8959e195
favour-22 <105045421+favour-22@users.noreply.github.com>
2023-06-10 03:54:41 +0100

    added more details

--- app.py
+++ app.py
@@ -2,26 +2,61 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Set secret key for app
 app.secret_key = '894ad1df46d08f691c788a0e3a5d1701'
+
+# Set database URI for SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# Create SQLAlchemy database object
 db = SQLAlchemy(app)
 
 
-@app.route('/')
-def index():
-    return render_template('register.html')
-
-
-# class to store user info
+# Define User class to store user information
 class User(db.Model):
+    """
+    A class used to represent a user in the database.
+
+    Attributes
+    ----------
+    id : int
+        The unique id of the user.
+    username : str
+        The username of the user.
+    password : str
+        The password of the user.
+    email : str
+        The email address of the user.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
-    Email = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
 
 
-# Class to store created tasks
+# Define Task class to store created tasks
 class Task(db.Model):
+    """
+    A class used to represent a task in the database.
+
+    Attributes
+    ----------
+    id : int
+        The unique id of the task.
+    title : str
+        The title of the task.
+    description : str
+        The description of the task.
+    due_date : str
+        The due date of the task.
+    priority : str
+        The priority level of the task (e.g. high, medium, low).
+    labels : str
+        The labels associated with the task.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(200))
@@ -30,32 +65,73 @@ class Task(db.Model):
     labels = db.Column(db.String(200))
 
     def __init__(self, title, description, due_date, priority, labels):
+        """
+        Initializes a new instance of the Task class.
+
+        Parameters
+        ----------
+        title : str
+            The title of the task.
+        description : str
+            The description of the task.
+        due_date : str
+            The due date of the task.
+        priority : str
+            The priority level of the task (e.g. high, medium, low).
+        labels : str
+            The labels associated with the task.
+        """
+
         self.title = title
         self.description = description
         self.due_date = due_date
         self.priority = priority
         self.labels = labels
 
+
+@app.route('/')
+def index():
+    """
+    Renders the registration page.
+    
+    Returns
+    -------
+    str
+        The rendered registration page.
+    """
+    return render_template('register.html')
+
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    Handles user registration.
+    
+    Returns
+    -------
+    str
+        The rendered login page if the registration is successful.
+        Otherwise, redirects to the login page with an error message.
+    """
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        Email = request.form['Email']
+        email = request.form['email']
+
         # Check if the username already exists
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
             flash('Username already exists', 'error')
             return redirect(url_for('login'))
-	 # Check if the email already exists
-        existing_user = User.query.filter_by(Email=Email).first()
+
+        # Check if the email already exists
+        existing_user = User.query.filter_by(email=email).first()
         if existing_user:
             flash('Email address already exists', 'error')
             return redirect(url_for('register'))
 
-
         # Create a new user
-        user = User(username=username, password=password,Email=Email)
+        user = User(username=username, password=password, email=email)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -64,6 +140,15 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    Handles user login.
+    
+    Returns
+    -------
+    str
+        The rendered successful page if the login is successful.
+        Otherwise, returns an error message.
+    """
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -71,12 +156,20 @@ def login():
         if user:
             return render_template('successful.html')
         else:
-            return 'Invalid username or password'
+            flash('Invalid username or password','error')
     return render_template('login.html')
 
 
 @app.route('/Todo', methods=['GET', 'POST'])
 def tasks():
+    """
+    Handles task creation and retrieval.
+    
+    Returns
+    -------
+    str
+        The rendered Todo page with all tasks retrieved fromthe database.
+    """
     if request.method == 'POST':
         title = request.form['taskTitle']
         description = request.form['taskDescription']
@@ -90,11 +183,12 @@ def tasks():
 
     tasks = Task.query.all()
     return render_template('Todo.html', tasks=tasks)
-	
 
 
 if __name__ == '__main__':
     with app.app_context():
+        # Create all database tables before running the app
         db.create_all()
-    app.run(debug=True,port=8000)
 
+    # Run the app in debug mode on port 8000
+    app.run(debug=True, port=8000)
