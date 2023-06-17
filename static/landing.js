da4fb3ff63bff142dc4cc720c26989c9cbfd491c
favour-22 <105045421+favour-22@users.noreply.github.com>
2023-06-17 18:09:08 +0100

    added changes

---
+++ static/landing.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('myForm');
+const notification = document.getElementById('notification');
+
+function showSuccess() {
+  notification.classList.remove('hidden');
+}
